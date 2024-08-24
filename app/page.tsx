7b56a6e771243d9427af161070a6be170b44d177
JalalLinuX <smjjalalzadeh93@gmail.com>
2024-08-24 13:13:54 +0330

    route login button to /dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,8 @@ export default function Page() {
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span className={`${lalezar.className} text-xl md:text-3xl md:leading-normal`}>ورود</span> <ArrowLeftIcon className="w-5 md:w-6" />
+            <Link href={'/dashboard'} className={`${lalezar.className} text-xl md:text-3xl md:leading-normal`}>ورود</Link>
+            <ArrowLeftIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
