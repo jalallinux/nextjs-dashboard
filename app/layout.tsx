e1618f06edc9b22c86fda6ba08b68de17a53edf3
JalalLinuX <smjjalalzadeh93@gmail.com>
2024-08-24 09:37:08 +0330

    complete to chapter-3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
+import '@/app/ui/global.css';
+import { vazirmatn } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="fa" dir="rtl">
+      <body className={`${vazirmatn.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Vazirmatn, Lalezar } from 'next/font/google';
+ 
+export const vazirmatn = Vazirmatn({ subsets: ['arabic'] });
+export const lalezar = Lalezar({ subsets: ['arabic'], weight: '400' });
